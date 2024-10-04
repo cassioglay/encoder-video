5c9042f135bac70264161c9f8f6972ad8d6053c9
cassioglay <cassio.glay#locktec.com.br>
2024-10-04 13:40:17 -0300

    feat: Video Upload - add service upload video to google

--- encoder/aplication/services/upload_manager.go
+++ encoder/aplication/services/upload_manager.go
@@ -3,8 +3,10 @@ package services
 import (
 	"context"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 
 	"cloud.google.com/go/storage"
@@ -32,7 +34,7 @@ func (vu *VideoUpload) uploadOject(objectPath string, client *storage.Client, ct
 		return err
 	}
 
-	f.Close()
+	defer f.Close()
 
 	//Connect to google cloud storage
 	wc := client.Bucket(vu.OutputBucket).Object(path[1]).NewWriter(ctx)
@@ -67,6 +69,60 @@ func (vu *VideoUpload) loadPaths() error {
 	}
 
 	return nil
+}
+
+func (vu *VideoUpload) ProcessUpload(concurrency int, doneUpload chan string) error {
+
+	in := make(chan int, runtime.NumCPU())
+	returnChannel := make(chan string)
+
+	err := vu.loadPaths()
+	if err != nil {
+		return err
+	}
+
+	uploadClient, ctx, err := getClientUpload()
+	if err != nil {
+		return err
+	}
+
+	for proces := 0; proces < concurrency; proces++ {
+		go vu.uploadWorker(in, returnChannel, uploadClient, ctx)
+	}
+
+	go func() {
+		for x := 0; x < len(vu.Paths); x++ {
+			in <- x
+		}
+		close(in)
+	}()
+
+	//Break all if exist error or upload completed
+	for r := range returnChannel {
+		if r != "" {
+			doneUpload <- r
+			break
+		}
+	}
+
+	return nil
+}
+
+func (vu *VideoUpload) uploadWorker(in chan int, returnChan chan string, uploadClient *storage.Client, ctx context.Context) {
+	for x := range in {
+		err := vu.uploadOject(vu.Paths[x], uploadClient, ctx)
+
+		if err != nil {
+			vu.Errors = append(vu.Errors, vu.Paths[x])
+			log.Printf("error during the upload: %v. Error: %v", vu.Paths[x], err)
+			returnChan <- err.Error()
+		}
+
+		returnChan <- "" // Done, not error to do upload video!
+	}
+
+	returnChan <- "upload completed"
+
 }
 
 func getClientUpload() (*storage.Client, context.Context, error) {
